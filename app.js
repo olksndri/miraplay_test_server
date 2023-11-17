fcd876cd882ae2124d744c12bdc9821f21016ac3
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2023-11-17 16:55:00 +0200

    added healthcheck

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const passport = require("passport");
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
 
-const { authRouter } = require("./src/routes/index");
+const { authRouter } = require('./src/routes/index');
 
 const app = express();
 
@@ -10,14 +10,18 @@ app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
-app.use("/auth", authRouter);
+app.use('/auth', authRouter);
+
+app.use('/health-check', (req, res) => {
+  res.status(200).json({ message: 'Ok' });
+});
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Internal server error" } = err;
+  const { status = 500, message = 'Internal server error' } = err;
   res.status(status).json({ message });
 });
 
